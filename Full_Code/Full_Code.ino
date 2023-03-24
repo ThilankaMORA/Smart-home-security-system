76cc7503b9c524e8b04cf4312a83cd90fe1c43eb
thilanka wickrama <81402568+ThilankaMORA@users.noreply.github.com>
2023-03-24 22:53:47 +0530

    test

---
+++ Full_Code/Full_Code.ino
@@ -0,0 +1,123 @@
+#include <Servo.h>
+#define up 3
+#define down 4
+#define left 5
+#define right 6
+#define center 7
+
+Servo servo1;
+Servo servo2;
+
+int pos1 = 90;
+int pos2 = 90;
+
+int calibrationTime = 30;        
+long unsigned int lowIn;         
+long unsigned int pause = 5000;  
+
+boolean lockLow = true;
+boolean takeLowTime;  
+
+int pirPin = 8;  
+int Buzzer = 11; 
+
+void setup() {
+  //Serial.begin(9600);
+  pinMode(center,INPUT);
+  pinMode(up,INPUT);
+  pinMode(down,INPUT);
+  pinMode(left,INPUT);
+  pinMode(right,INPUT);
+
+  pinMode(pirPin, INPUT);
+  pinMode(Buzzer, OUTPUT);
+  digitalWrite(pirPin, LOW);
+
+  servo1.attach(9);
+  servo2.attach(10);
+  servo1.write(pos1);
+  delay(200);
+  servo2.write(pos2);
+  delay(200);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  byte upstate = digitalRead(up);
+  if ((upstate == HIGH) && (pos1 <= 160)){
+    //Serial.println("Button up is pressed");
+    pos1+=10;
+    servo1.write(pos1);
+    //Serial.println(pos1);
+    //Serial.println(pos2);
+    
+    delay(250);
+    }
+  byte downstate = digitalRead(down);
+  if ((downstate == HIGH) && (pos1 >= 20)){
+    //Serial.println("Button down is pressed");
+    pos1-=10;
+    servo1.write(pos1);
+    
+    delay(250);
+    }
+  byte leftstate = digitalRead(left);
+  if ((leftstate == HIGH) && (pos2 <= 160)){
+    //Serial.println("Button left is pressed");
+    pos2+=10;
+    servo2.write(pos2);
+    
+    delay(250);
+    }
+  byte rightstate = digitalRead(right);
+  if ((rightstate == HIGH) && (pos2 >= 20)){
+    //Serial.println("Button right is pressed");
+    pos2-=10;
+    servo2.write(pos2);
+    
+    delay(250);
+    }
+  byte centerstate = digitalRead(center);
+  if (centerstate == HIGH){
+    //Serial.println("Button center is pressed");
+    pos1=90;
+    pos2=90;
+    servo1.write(pos1);
+    delay(200);
+    servo2.write(pos1);
+    delay(200);
+    }
+
+    if(digitalRead(pirPin) == HIGH){
+       tone(Buzzer,500);
+       if(lockLow){  
+         lockLow = false;            
+         //Serial.println("---");
+         //Serial.print("motion detected at ");
+         //Serial.print(millis()/1000);
+         //Serial.println(" sec"); 
+         delay(50);
+         }         
+         takeLowTime = true;
+       }
+
+     if(digitalRead(pirPin) == LOW){
+       noTone(Buzzer);
+       if(takeLowTime){
+        lowIn = millis();         
+        takeLowTime = false; 
+        }
+       //if the sensor is low for more than the given pause, 
+       //we assume that no more motion is going to happen
+       if(!lockLow && millis() - lowIn > pause){  
+           lockLow = true;                        
+           //Serial.print("motion ended at ");      //output
+           //Serial.print((millis() - pause)/1000);
+           //Serial.println(" sec");
+           delay(50);
+           }
+       }
+  
+  }
+
